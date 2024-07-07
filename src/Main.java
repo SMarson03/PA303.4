952a48095cb6244450832f92d680e7c5a76f9679
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-07-07 14:36:42 -0400

    3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+//import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //1. Write a program that declares 1 integer variable x, and then
@@ -5,28 +6,67 @@ public class Main {
         // if x is less than 10. Change x to equal 15, and notice the result
         // (console should not display anything).
 
-        int x = 7;
-        LessThan10(x);
+//        int x = 7;
+//        LessThan10(x);
+//
+//        x= 15;
+//        LessThan10(x);
+//    }
+//    public static void LessThan10(int x) {
+//        if (x < 10){
+//            System.out.println("Less than 10");
+//        }
+//
+//    }
 
-        x= 15;
-        LessThan10(x);
+        LessThan10(7);
+        LessThan10(15);
+        LessThanOrGreaterThan10(7);
+        LessThanOrGreaterThan10(15);
+//        Between10And20(15);
+//        IsInRange(15);
+//        IsInRange(5);
+
+        int grade, day, salary;
+//        Scanner scan = new Scanner(System.in);
+//        grade = scan.nextInt();
+//        day = scan.nextInt();
+//        scan.nextLine();
+//        String FilingStatus;
+//        FilingStatus = scan.nextLine();
+//        salary = scan.nextInt();
+//        scan.close();
+//        GetLetterGrade(grade);
+//        WeekdayName(day);
+//        GetTax(FilingStatus, salary);
     }
+
     public static void LessThan10(int x) {
         if (x < 10){
-            System.out.println("Less than 10");
+            System.out.println("LessThan10");
         }
 
     }
 
-    //2. Write a program that declares 1 integer variable x, and then assigns 7 to it.
+        //2. Write a program that declares 1 integer variable x, and then assigns 7 to it.
     // Write an if-else statement that prints out “Less than 10” if x is less than 10, and
     // “Greater than 10” if x is greater than 10. Change x to 15 and notice the result.
-    public static void GreaterOrLesserThan10(String[] args) {
+        public static void LessThanOrGreaterThan10(int x) {
+        if (x < 10){
+            System.out.println("Less than 10");
+        }else if (x > 10){
+            System.out.println("Greater than 10");
+        }
+      }
 
+        //3. Write a program that declares 1 integer variable x, and then assigns 15 to it.
+    // Write an if-else-if statement that prints out “Less than 10” if x is less than 10;
+    // “Between 10 and 20” if x is greater than 10 but less than 20, and “Greater than or equal
+    // to 20” if x is greater than or equal to 20. Change x to 50 and notice the result.
+
+    public static void main(String[] args) {
+        
     }
-        //
-        //3. Write a program that declares 1 integer variable x, and then assigns 15 to it. Write an if-else-if statement that prints out “Less than 10” if x is less than 10; “Between 10 and 20” if x is greater than 10 but less than 20, and “Greater than or equal to 20” if x is greater than or equal to 20. Change x to 50 and notice the result.
-        //
         //4. Write a program that declares 1 integer variable x, and then assigns 15 to it. Write an if-else statement that prints “Out of range” if the number is less than 10 or greater than 20 and prints “In range” if the number is between 10 and 20 (including equal to 10 or 20). Change x to 5 and notice the result.
         //
         //5. Write a program that uses if-else-if statements to print out grades A, B, C, D, F, according to the following criteria:
